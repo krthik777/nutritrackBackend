ee152ea9f7b1e82b4c473ca671a02599cea6fc5e
krthik777 <karthikv9778@gmail.com>
2025-02-16 01:29:55 +0530

    bkaa

--- server.js
+++ server.js
@@ -153,5 +153,5 @@ app.get('/api/hasdetails', async (req, res) => {
 
 
 // Server Listening
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
